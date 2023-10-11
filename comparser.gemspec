583c2e55495162d691cabd0f6cf88815595995df
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-10-11 11:32:25 -0300

    remove zeitwerk dependency

--- comparser.gemspec
+++ comparser.gemspec
@@ -28,6 +28,4 @@ Gem::Specification.new do |spec|
   spec.bindir = "exe"
   spec.executables = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
-
-  spec.add_dependency "zeitwerk", "~> 2.6"
 end
--- lib/comparser.rb
+++ lib/comparser.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
-require "zeitwerk"
-
-loader = Zeitwerk::Loader.for_gem
-loader.setup
-
-module Comparser; end
-
-loader.eager_load
+require_relative "comparser/version"
+require_relative "comparser/parser"
+require_relative "comparser/parser/result"
+require_relative "comparser/parser/state"
+require_relative "comparser/parser/step"
