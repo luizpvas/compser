ea59a598d51be7dcdffc60e593c6d3390f371f2a
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-10-12 10:31:22 -0300

    refactor specs

--- test/comparser/parser/test_one_of.rb
+++ test/comparser/parser/test_one_of.rb
@@ -5,24 +5,8 @@ require "test_helper"
 class Comparser::Parser::TestOneOf < Minitest::Test
   include Comparser::Parser
 
-  def integer
-    is_digit = ->(ch) { ch.match?(/\d/) }
-
-    chomp_if(error_message: "expected digit", is_good: is_digit)
-      ._ chomp_while(is_good: is_digit)
-      ._ and_then(to_value: ->(state) { state.good! Integer(state.consume_chomped) })
-  end
-
-  def keyword
-    is_lowercase_char = ->(ch) { ch.match?(/[a-z]/) }
-
-    chomp_if(error_message: "expected keyword", is_good: is_lowercase_char)
-      ._ chomp_while(is_good: is_lowercase_char)
-      ._ and_then(to_value: ->(state) { state.good! state.consume_chomped })
-  end
-
   def integer_or_keyword
-    one_of [integer, keyword]
+    one_of [integer, keyword("foo")]
   end
 
   def test_one_of_when_first_branch_succeeds
@@ -33,16 +17,16 @@ class Comparser::Parser::TestOneOf < Minitest::Test
   end
 
   def test_one_of_when_second_branch_succeeds
-    result = parse("abc", integer_or_keyword)
+    result = parse("foo", integer_or_keyword)
 
     assert result.good?
-    assert_equal "abc", result.value
+    assert_equal "foo", result.value
   end
 
   def test_one_of_when_no_branch_succeeds
     result = parse("ABC", integer_or_keyword)
 
     assert result.bad?
-    assert_equal "expected keyword", result.message
+    assert_equal 'expected keyword "foo"', result.message
   end
 end
