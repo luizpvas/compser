dca76a894b4c6ddd3ddb28edd546a8c2d0b0c5cb
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-10-13 16:50:59 -0300

    clean up

--- examples/json.rb
+++ examples/json.rb
@@ -1,14 +1,6 @@
 module MyJson
-  include Comparser::Parser
+  include Comparser
 
   def expression
-    one_of [ double_quoted_string, decimal, boolean ]
-  end
-
-  def boolean
-    one_of [
-      map(1, true) + keyword("true"),
-      map(1, false) + keyword("false")
-    ]
   end
 end
