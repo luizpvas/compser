27aaaf9beb7968c2b864fa5c660d872da225b01f
Luiz Vasconcellos <luizpvasc@gmail.com>
2023-10-13 11:33:48 -0300

    bring back state implementation

--- lib/comparser.rb
+++ lib/comparser.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 
 require_relative "comparser/version"
+require_relative "comparser/result"
+require_relative "comparser/savepoint"
+require_relative "comparser/state"
+require_relative "comparser/step"
+
+module Comparser
+  def succeed
+    Step.new
+  end
+end
---
+++ lib/comparser/step.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class Comparser::Step
+  def initialize
+  end
+end
